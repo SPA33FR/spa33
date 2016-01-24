7231c16195251753f874309555a0c379f961e149
Hannibal974 <romain.poussin@ynov.com>
2016-01-24 12:30:31 +0100

    correctif by delete all

--- README.md
+++
@@ -1,4 +0,0 @@
-spa33
-=====
-
-A Symfony project created on January 13, 2016, 4:34 pm.
--- src/SPA/SpaBundle/SPASpaBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SPA\SpaBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SPASpaBundle extends Bundle
-{
-}
