a203cffb4003b2520705458ed538be06958b6724
Hannibal974 <romain.poussin@ynov.com>
2016-01-27 17:07:31 +0100

    Implement FOSUserBundle + login  + redirect formulaire articles + navbar

--- src/Spa/SpaBundle/Controller/AdminController.php
+++ src/Spa/SpaBundle/Controller/AdminController.php
@@ -4,9 +4,17 @@ namespace Spa\SpaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class AdminController extends Controller
-{
-     public function indexAction() {
-         return $this->render('SpaSpaBundle:Admin:index.html.twig', array());
-     } 
+class AdminController extends Controller {
+
+    public function indexAction() {
+        return $this->render('SpaSpaBundle:Admin:index.html.twig', array());
+    }
+
+    public function configurateArticlesAction() {
+        $em = $this->getDoctrine()->getManager();
+        $article = new \Spa\SpaBundle\Entity\Articles();
+        $articles = $article->getAllArticles($em);
+        return $this->render('SpaSpaBundle:Admin:articles.html.twig', array("articles" => $articles));
+    }
+
 }
